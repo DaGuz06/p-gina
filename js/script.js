00f276f47d01c10eba9f97d386ef3ae6b667de7e
D4Guz <146863049+DaGuz06@users.noreply.github.com>
2024-10-18 23:47:59 +0200

    cambios

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-
+//menu desplegable
 document.querySelector('.menu-icon').addEventListener('click', () => {
     const mobileMenu = document.querySelector('.mobile-menu');
     mobileMenu.classList.toggle('open');
 });
+
+//carrusel
+let slideIndex = 0;
+mostrarSlides();
+
+function cambiarSlide(n) {
+    slideIndex += n;
+    mostrarSlides();
+}
+
+function mostrarSlides() {
+    let slides = document.getElementsByClassName("slide");
+    if (slideIndex >= slides.length) { slideIndex = 0; }
+    if (slideIndex < 0) { slideIndex = slides.length - 1; }
+    
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    
+    slides[slideIndex].style.display = "block";  
+}
