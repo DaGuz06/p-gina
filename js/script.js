b5747551253d64b0108abbaf5ec7b23079d56500
D4Guz <146863049+DaGuz06@users.noreply.github.com>
2024-10-18 14:04:26 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+
+document.querySelector('.menu-icon').addEventListener('click', () => {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.classList.toggle('open');
+});
